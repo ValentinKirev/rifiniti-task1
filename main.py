90e8552f2d8fbe7a707577281aef2dc54e5429dd
Valentin Kirev <valentinkirev666@gmail.com>
2024-12-22 12:47:31 +0200

    added logic for printing the count of unique values in 'species' column

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 # load data as Pandas DataFrame
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 columns_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
@@ -9,4 +8,9 @@ data_frame = pd.read_csv(url, names=columns_names)
 
 # Print Summary statistics for each feature
 print("Summary statistics: ")
-print(data_frame.describe())
+print(f"{data_frame.describe()}\n")
+
+
+# Print count of unique values in the "species" column
+print("Count of unique values in the 'species' column: ")
+print(f"{data_frame['species'].value_counts()}\n")
