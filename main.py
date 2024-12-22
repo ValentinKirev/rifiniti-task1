6b3d6d7494f634bd049d8f3f4017d5424e0f4102
Valentin Kirev <valentinkirev666@gmail.com>
2024-12-22 12:39:11 +0200

    added columns names into DataFrame

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 # load data as Pandas DataFrame
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+columns_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
 
-data_frame = pd.read_csv(url)
+data_frame = pd.read_csv(url, names=columns_names)
 
 # Print Summary statistics for each feature
 print("Summary statistics: ")
